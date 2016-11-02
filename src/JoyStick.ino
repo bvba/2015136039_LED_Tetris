7b47773cb03f6bbb7b99ab5b1db24167ee6a95ee
bvba <bvba@koreatech.ac.kr>
2016-11-03 04:30:24 +0900

    Update JoyStick.ino

--- src/JoyStick.ino
+++ src/JoyStick.ino
@@ -1,8 +1,15 @@
-#define UP 1
-#define LEFT 2
-#define DOWN 3
-#define RIGHT 4
-#define ON 5
+#define UP          1
+#define UP_LEFT     2
+#define LEFT        3
+#define DOWN_LEFT   4
+#define DOWN        5
+#define DOWN_RIGTH  6
+#define RIGHT       7
+#define UP_RIGTH    8
+#define ON          9
+#define joyStickX   A4
+#define joyStickY   A5
+#define joyStickSW  2
 
 int joyStickX = A4;
 int joyStickY = A5;
@@ -20,15 +27,24 @@ void loop() {
   Serial.print(joyStick());
 }
 
-int joyStick() {
+int joyStick() {  // 조이스틱의 입력값을 반환
   int x = analogRead(joyStickX);
   int y = analogRead(joyStickY);
-  int sw = digitalRead(joyStickSw);
-  if(x == 0) return LEFT;
-  else if(x == 1023) return RIGHT;
-  else if(y == 0) return UP;
-  else if(y == 1023) return DOWN;
-  else if(sw == 0) return ON;
-  else return 0;
+  int sw = digitalRead(joyStickSW);
+  if(x <= 200) {
+    if(y <= 200)        return UP_LEFT;
+    else if(y >= 800)   return DOWN_LEFT;
+    else                return LEFT;
+  }
+  else if(x >= 800) {
+    if(y <= 200)        return UP_RIGTH;
+    else if(y >= 800)   return DOWN_RIGTH;
+    else                return RIGHT;
+  }
+  else {
+    if(y <= 200)        return UP;
+    else if(y >= 800)   return DOWN;
+  }
+  if(sw == 0)           return ON;
+  return 0;
 }
-
